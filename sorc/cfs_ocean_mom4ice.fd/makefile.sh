da5db71e42b57fe3cfe15c1012b439069728d120
jack.woollen <jack.woollen@noaa.gov>
2019-08-18 14:10:42 +0000

    mods to compile the coupled model using smpi
    Former-commit-id: c458da933bb67e115e8e9a4f5ca3dc5c6e784fd1

--- sorc/cfs_ocean_mom4ice.fd/makefile.sh
+++ sorc/cfs_ocean_mom4ice.fd/makefile.sh
@@ -3,8 +3,23 @@
 
 set +x
 echo
-module unload impi/18.0.1
+module purge
+module load EnvVars/1.0.2
+module load ips/18.0.1.163
 module load smpi/10.1.1.0
+module load ESMF/4_0_0rp2
+module load NetCDF/3.6.3
+module load bacio/2.0.2
+module load nemsio/2.2.3
+module load sp/2.0.2
+module load w3emc/2.3.0
+module load w3nco/2.0.6
+module load bufr/11.2.0
+module load ip/3.0.1
+module load sfcio/1.0.0
+module load sigio/2.0.1
+module load gfsio/1.1.0
+module load landsfcutil/2.1.0
 module list
 echo
 set -x
