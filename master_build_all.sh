a2025cc94fa174957f9ca638f377c97f33dcabdc
jack.woollen <jack.woollen@noaa.gov>
2019-09-19 10:09:23 +0000

    build correction for CRTMv206
    Former-commit-id: 7ca4d01f0d5d8c00997301d42657ef76683d3508

--- master_build_all.sh
+++ master_build_all.sh
@@ -4,7 +4,7 @@ set -euax
 export REPOROOT=$PWD  
 export WRFPATH=/gpfs/dell2/emc/modeling/noscrub/Jack.Woollen/wrf_shared.v1.1.0
 
-[[ $# -eq 0 || $1 = 206 ]] && { cd $REPOROOT/global_shared_cfs/CRTMv206; build; }
+[[ $# -eq 0 || $1 = 206 ]] && { cd $REPOROOT/global_shared_cfs/CRTMv206/sorc; . configure/ifort.setup; cd ..; build; }
 
 [[ $# -eq 0 || $1 = cfs ]] && { cd $REPOROOT/sorc; mkdir -p ../exec; build_all.dell; }
 
