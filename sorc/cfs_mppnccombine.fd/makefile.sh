f68e41df4cc713bfe1c9e944f11d2f92c71fc995
jack.woollen <jack.woollen@noaa.gov>
2019-12-06 01:22:47 +0000

    fixed combine for smpi + misc fcst script cleanup
    Former-commit-id: 5aa5f30de303f652f4364fd8988e80bc5a7dc9ee

--- sorc/cfs_mppnccombine.fd/makefile.sh
+++ sorc/cfs_mppnccombine.fd/makefile.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
-##module load NetCDF-cray-sandybridge/3.6.3
-
+set +x
+echo
+module purge
+module load EnvVars/1.0.2
+module load ips/18.0.1.163
+module load smpi/10.1.1.0
+module load NetCDF/3.6.3
+module list
+echo
 set -ex
+
 
 export CFLAGSM="-O3 -xHost"
 export LDFLAGSM=
 
 export NCDF="-L$NETCDF/lib -lnetcdf"
 export INC=-I$NETCDF/include
-export CC=icc
+export CC=mpicc
 
 echo; make=`basename $PWD`
 echo make-ing ${make%.*}
@@ -23,6 +31,6 @@ rm -f *.o *.mod
 
 #make the mpi wrapper
 
-mpiifort -traceback -g -o cfs_mpinccombine mpinccombine.f
+mpif90 -traceback -g -o cfs_mpinccombine mpinccombine.f
 mv cfs_mpinccombine ../../exec
 rm -f *.o *.mod
