3e82b74f3d2a1c336be23dc415a0f439907cb3eb
jack.woollen <jack.woollen@noaa.gov>
2020-03-13 17:26:22 +0000

    updating JISNI to provide grib files for public dissemination
    Former-commit-id: d5a1ff74e4d83342cf811c3bd49b6e682fb58d05

--- ush/cfs_snowdepth.sh
+++ ush/cfs_snowdepth.sh
@@ -5,23 +5,26 @@ set -uax
 ######################################
 # Specify directory paths for this job
 ######################################
-export DCOMROOT=${DCOMROOT:-/dcom}   #Luke adds it for test
-export USERDIR=${DCOMROOT}
-export OUTDIR=${DCOMROOT}
+
 export TANKDIR=${DCOMROOT}
-export OUTDIR=${DCOMROOT}
 
-export HOMEgrib=${HOMEgrib:-$HOMEcfs/EMC_land_utilities}
-export USHgrib=${USHgrib:-$HOMEgrib/ush}
-export EXECgrib=${EXECgrib:-$HOMEgrib/exec}
-export FIXgrib=${FIXgrib:-$HOMEgrib/fix}
+export COMOUT=$COMROOT/${NET}/${envir}/${RUN}.${PDY}
+export COMOLD=$COMROOT/${NET}/${envir}/${RUN}.${PDYm1}
 
+export HOMEgrib=$HOMEcfs/EMC_land_utilities
+export EXECgrib=$HOMEgrib/exec
+export USHgrib=$HOMEgrib/ush
+export FIXgrib=$HOMEgrib/fix
+
+# run the JISNI procedure
+# -----------------------
 
 $HOMEgrib/scripts/exsnowgrib.sh.ecf
 
-# try yesterday if today is missing
+# copy snowdepth files to cfs filenames
+# -------------------------------------
 
-if [[ $? -ne 0 ]]; then
-  export PDY=$PDYm1
-  $HOMEgrib/scripts/exsnowgrib.sh.ecf
-fi
+cp $COMOUT/snowdepth.global.grb  $COMOUT/${RUN}1.${cycle}.snogrb
+cp $COMOUT/snowdepth.t574.grb    $COMOUT/${RUN}1.${cycle}.snogrb_t574
+cp $COMOUT/snowdepth.t382.grb    $COMOUT/${RUN}1.${cycle}.snogrb_t382
+
