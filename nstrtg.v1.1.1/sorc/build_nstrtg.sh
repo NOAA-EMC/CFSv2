afbe4ec681bd4270e52c2561d829b594fac55d66
Jack Woollen <Jack.Woollen@alogin02.acorn.wcoss2.ncep.noaa.gov>
2021-08-24 13:49:02 +0000

    changes to  nstrtg.v1.1.1 for building on wcoss2

--- nstrtg.v1.1.1/sorc/build_nstrtg.sh
+++ nstrtg.v1.1.1/sorc/build_nstrtg.sh
@@ -3,9 +3,11 @@ set -eux
 
 mkdir -p ../exec # place for executables
 
+set +x
 module purge
-source ../modulefiles/nstrtg.wcoss_dell_p3  
+source ../modulefiles/nstrtg.wcoss2 
 module list
+set -x
 
 cd ./nstrtg.fd
 ./makefile.sh
