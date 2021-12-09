572e6027619621245387cfa0d8341dc10dc0dd26
Jack Woollen <Jack.Woollen@noaa.gov>
2021-12-09 08:42:40 +0000

    moving GESdir to COMOUT

--- scripts/excfs_cdas_godas.sh
+++ scripts/excfs_cdas_godas.sh
@@ -393,7 +393,7 @@ fi
 
 if [ $SENDCOM = YES ] ; then
   cp ocnanl.$CDUMP.$CDATE.tar $COMOUT/${RUN1}.t${cyc}z.ocnanl.tar
-  cp ocnanl.$CDUMP.$CDATE.tar $GESdir/${RUN1}.t${cyc}z.ocnanl.tar
+##cp ocnanl.$CDUMP.$CDATE.tar $GESdir/${RUN1}.t${cyc}z.ocnanl.tar
 fi
 
 #-------------------------------------------------------------------------------
